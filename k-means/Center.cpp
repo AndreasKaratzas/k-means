056c927221bd9fab3fac3491ad6f3f01622c714e
Andreas <42451412+andreasceid@users.noreply.github.com>
2020-11-21 21:22:11 +0200

    Identation fixed.

--- k-means/Center.cpp
+++ k-means/Center.cpp
@@ -11,13 +11,13 @@
  */
 void init_centers(const std::vector<std::array<float, Nv>>& Vec, std::vector<std::array<float, Nv>>& old_Center)
 {
-	std::vector<int> Random(N);																///< Declares our random `Vec` generator vector
+	std::vector<int> Random(N);														///< Declares our random `Vec` generator vector
 	std::iota(Random.begin(), Random.end(), 0);												///< Initializes vector with all possible indeces of `Vec`
-	std::shuffle(Random.begin(), Random.end(), std::mt19937{ std::random_device{}() });		///< Shuffles `Random` vector
+	std::shuffle(Random.begin(), Random.end(), std::mt19937{ std::random_device{}() });							///< Shuffles `Random` vector
 	for (int i = 0; i < Nc; i += 1)
 	{
-		std::array<float, Nv> Element;														///< Declares a vector `Elements` that temporarily holds the vector chosen from `Vec`					
-		Element = Vec.at(Random.at(i));														///< Copies contents of the random `Vec` element
-		old_Center.emplace_back(Element);													///< Moves `Element` variable to the `old_Center` variable
+		std::array<float, Nv> Element;													///< Declares a vector `Elements` that temporarily holds the vector chosen from `Vec`					
+		Element = Vec.at(Random.at(i));													///< Copies contents of the random `Vec` element
+		old_Center.emplace_back(Element);												///< Moves `Element` variable to the `old_Center` variable
 	}
 }
