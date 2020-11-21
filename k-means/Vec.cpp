a6bb008c628f4c7a6bd87ff45937be172b277f1f
Andreas <42451412+andreasceid@users.noreply.github.com>
2020-11-21 21:30:35 +0200

    Identation fixed.

--- k-means/Vec.cpp
+++ k-means/Vec.cpp
@@ -12,16 +12,16 @@
  */
 void vec_init(std::vector<std::array<float, Nv>>& Vec)
 {
-	std::random_device rd_Vec;												///< Initializes a random generator
-	std::mt19937 mt_Vec(rd_Vec());											///< Uses the mt19937 engine
+	std::random_device rd_Vec;						///< Initializes a random generator
+	std::mt19937 mt_Vec(rd_Vec());						///< Uses the mt19937 engine
 	std::uniform_real_distribution<float> dist_Vec(0.0, MAX_LIMIT + 0.0);	///< Generates a uniform distribution bounded by the `MAX_LIMIT` set in `Common.h` to prevent number overflow
 	for (int i = 0; i < N; i += 1)
 	{
-		std::array<float, Nv> Elements;										///< Declares a vector `Elements` that temporarily holds the vector that is to be inserted to `Vwc`
+		std::array<float, Nv> Elements;					///< Declares a vector `Elements` that temporarily holds the vector that is to be inserted to `Vwc`
 		for (int j = 0; j < Nv; j += 1)
 		{
-			Elements[j] = dist_Vec(mt_Vec);									///< Updates contents of `Elements`
+			Elements[j] = dist_Vec(mt_Vec);				///< Updates contents of `Elements`
 		}
-		Vec.emplace_back(Elements);											///< Moves `Elements` to `Vec`
+		Vec.emplace_back(Elements);					///< Moves `Elements` to `Vec`
 	}
 }
