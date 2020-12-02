00d547a99eaadace6f059f95bdda19b2ff49520c
Andreas <42451412+andreasceid@users.noreply.github.com>
2020-12-02 10:31:26 +0200

    Bug fix.

--- png2gif/pnf2gif.bat
+++ png2gif/pnf2gif.bat
@@ -1,3 +1,3 @@
 ffmpeg -framerate 1 -i kmeans%1d.png -c:v libx264 -r 30 temp.mp4
 ffmpeg -i temp.mp4 -filter_complex "[0:v] palettegen" template.png
-ffmpeg -i temp.mp4 -i template.png -r 12 -s 640x480 -filter_complex "[0:v][1:v] paletteuse" kmeans.gif
+ffmpeg -i temp.mp4 -i template.png -r 30 -filter_complex "[0:v][1:v] paletteuse" kmeans.gif
